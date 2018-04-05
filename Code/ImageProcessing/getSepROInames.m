70e5efa5062803cf6b411e786252fc589e36a056
Martin Vallières <mart.vallieres@gmail.com>
2018-04-05 17:13:53 +0200

    Daily work: working towards modification of + and - parsing in roiNames.csv files

--- Code/ImageProcessing/getSepROInames.m
+++ Code/ImageProcessing/getSepROInames.m
@@ -25,21 +25,41 @@ function [nameROIout,vectPlusMinus] = getSepROInames(nameROIin,delimiters)
 % Martin Vallieres for this matter.
 % -------------------------------------------------------------------------
 
-% FOR DELIMITERS "+" and "-"
+% WORKS ONLY FOR DELIMITERS "+" and "-"
+
+% FINDING "+" and "-"
 indPlus = strfind(nameROIin,delimiters{1}); vectPlus = ones(1,numel(indPlus));
 indMinus = strfind(nameROIin,delimiters{2}); vectMinus = ones(1,numel(indMinus))*-1;
 [ind,seq] = sort([indPlus,indMinus]);
-vectPlusMinus = [vectPlus,vectMinus]; vectPlusMinus = vectPlusMinus(seq);
+vectPlusMinus = [vectPlus,vectMinus]; vectPlusMinus = vectPlusMinus(seq); nDelim = numel(vectPlusMinus);
+
+% MAKING SURE "+" and "-" ARE NOT INSIDE A ROIname
+indStart = strfind(nameROIin,'{'); nROI = numel(indStart);
+indStop = strfind(nameROIin,'}');
+indKeep = true(1,nDelim);
+for d = 1:nDelim
+    for r = 1:nROI
+        if (indStop(r) - ind(d)) > 0 && (ind(d) - indStart(r)) > 0 % Thus inside a ROI name
+            indKeep(d) = false;
+            break
+        end
+    end
+end
+ind = ind(indKeep);
+vectPlusMinus = vectPlusMinus(indKeep);
+
+
+% PARSING ROI NAMES
 if isempty(ind)
-    nameROIout = {nameROIin};
+    nameROIout = {nameROIin(2:end-1)}; % Excluding the "{" and "}" at the start and end of the ROIname
 else
     nInd = numel(ind);
     nameROIout = cell(1,nInd+1);
-    nameROIout{1} = nameROIin(1:(ind(1)-1));
+    nameROIout{1} = nameROIin(2:(ind(1)-2)); % Excluding the "{" and "}" at the start and end of the ROIname
     for i = 2:nInd
-        nameROIout{i} = nameROIin((ind(i-1)+1):(ind(i)-1));
+        nameROIout{i} = nameROIin((ind(i-1)+2):(ind(i)-2)); % Excluding the "{" and "}" at the start and end of the ROIname
     end
-    nameROIout{nInd+1} = nameROIin((ind(end)+1):end);
+    nameROIout{nInd+1} = nameROIin((ind(end)+2):end-1); % Excluding the "{" and "}" at the start and end of the ROIname
 end
 
 end
